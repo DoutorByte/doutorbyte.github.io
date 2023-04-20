90da0155500504901d8e7d15101f7ce88064fc03
Doutor Byte <130619103+DoutorByte@users.noreply.github.com>
2023-04-20 15:15:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ $(document).ready(function(){
      });
 
      var typed = new Typed(".typing", {
-         strings:["DBA", "Analista de TI", "Desenvolvedor", "Designer", "Professor"],
+         strings:["DBA", "Profissional de TI", "Desenvolvedor", "Criado de conteúdo", "Professor"],
          typeSpeed:100,
          backSpeed:60,
          loop:true
      });
      var typed = new Typed(".typing-2", {
-        strings:["DBA", "Analista de TI", "Desenvolvedor", "Designer", "Professor"],
+        strings:["DBA", "Profissional de TI", "Desenvolvedor", "Criado de conteúdo", "Professor"],
         typeSpeed:100,
         backSpeed:60,
         loop:true
